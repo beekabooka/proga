e1245f960b2b942861e8a41784b86eb9bb3d8460
beekabooka <gromovk587@gmail.com>
2025-06-17 09:05:36 +0300

    Create AddMaterialForm.Designer.cs

---
+++ AddMaterialForm.Designer.cs
@@ -0,0 +1,225 @@
+namespace DemoGKV44
+{
+    partial class AddMaterialForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.cmbMaterialType = new System.Windows.Forms.ComboBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblStockQuantity = new System.Windows.Forms.Label();
+            this.txtStockQuantity = new System.Windows.Forms.TextBox();
+            this.lblMinQuantity = new System.Windows.Forms.Label();
+            this.txtMinQuantity = new System.Windows.Forms.TextBox();
+            this.lblPackageQuantity = new System.Windows.Forms.Label();
+            this.txtPackageQuantity = new System.Windows.Forms.TextBox();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(20, 20);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(121, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Наименование материала:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(23, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(300, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(20, 70);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(79, 13);
+            this.lblType.TabIndex = 2;
+            this.lblType.Text = "Тип материала:";
+            // 
+            // cmbMaterialType
+            // 
+            this.cmbMaterialType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMaterialType.FormattingEnabled = true;
+            this.cmbMaterialType.Location = new System.Drawing.Point(23, 86);
+            this.cmbMaterialType.Name = "cmbMaterialType";
+            this.cmbMaterialType.Size = new System.Drawing.Size(300, 21);
+            this.cmbMaterialType.TabIndex = 3;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(20, 120);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(109, 13);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Цена единицы (руб):";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(23, 136);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // lblStockQuantity
+            // 
+            this.lblStockQuantity.AutoSize = true;
+            this.lblStockQuantity.Location = new System.Drawing.Point(20, 170);
+            this.lblStockQuantity.Name = "lblStockQuantity";
+            this.lblStockQuantity.Size = new System.Drawing.Size(107, 13);
+            this.lblStockQuantity.TabIndex = 6;
+            this.lblStockQuantity.Text = "Количество на складе:";
+            // 
+            // txtStockQuantity
+            // 
+            this.txtStockQuantity.Location = new System.Drawing.Point(23, 186);
+            this.txtStockQuantity.Name = "txtStockQuantity";
+            this.txtStockQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtStockQuantity.TabIndex = 7;
+            // 
+            // lblMinQuantity
+            // 
+            this.lblMinQuantity.AutoSize = true;
+            this.lblMinQuantity.Location = new System.Drawing.Point(20, 220);
+            this.lblMinQuantity.Name = "lblMinQuantity";
+            this.lblMinQuantity.Size = new System.Drawing.Size(112, 13);
+            this.lblMinQuantity.TabIndex = 8;
+            this.lblMinQuantity.Text = "Минимальное количество:";
+            // 
+            // txtMinQuantity
+            // 
+            this.txtMinQuantity.Location = new System.Drawing.Point(23, 236);
+            this.txtMinQuantity.Name = "txtMinQuantity";
+            this.txtMinQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtMinQuantity.TabIndex = 9;
+            // 
+            // lblPackageQuantity
+            // 
+            this.lblPackageQuantity.AutoSize = true;
+            this.lblPackageQuantity.Location = new System.Drawing.Point(20, 270);
+            this.lblPackageQuantity.Name = "lblPackageQuantity";
+            this.lblPackageQuantity.Size = new System.Drawing.Size(121, 13);
+            this.lblPackageQuantity.TabIndex = 10;
+            this.lblPackageQuantity.Text = "Количество в упаковке:";
+            // 
+            // txtPackageQuantity
+            // 
+            this.txtPackageQuantity.Location = new System.Drawing.Point(23, 286);
+            this.txtPackageQuantity.Name = "txtPackageQuantity";
+            this.txtPackageQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtPackageQuantity.TabIndex = 11;
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Location = new System.Drawing.Point(20, 320);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(95, 13);
+            this.lblUnit.TabIndex = 12;
+            this.lblUnit.Text = "Единица измерения:";
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(23, 336);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(100, 20);
+            this.txtUnit.TabIndex = 13;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(167, 380);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 30);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(248, 380);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 30);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // AddMaterialForm
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(350, 430);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.txtPackageQuantity);
+            this.Controls.Add(this.lblPackageQuantity);
+            this.Controls.Add(this.txtMinQuantity);
+            this.Controls.Add(this.lblMinQuantity);
+            this.Controls.Add(this.txtStockQuantity);
+            this.Controls.Add(this.lblStockQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.cmbMaterialType);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddMaterialForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Добавление материала";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.ComboBox cmbMaterialType;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblStockQuantity;
+        private System.Windows.Forms.TextBox txtStockQuantity;
+        private System.Windows.Forms.Label lblMinQuantity;
+        private System.Windows.Forms.TextBox txtMinQuantity;
+        private System.Windows.Forms.Label lblPackageQuantity;
+        private System.Windows.Forms.TextBox txtPackageQuantity;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
